770d9c55108092b0755379ed6f7a57a3af525d70
Thibaut Pérami <thibaut.perami@cl.cam.ac.uk>
2022-09-09 13:41:14 +0200

    Update register type for handling unknown
    This new register type is VERY experimental and subject to
    frequent change

--- ISASem/ArmInst.v
+++ ISASem/ArmInst.v
@@ -1,12 +1,29 @@
 Require Import Strings.String.
 Require Import stdpp.unstable.bitvector.
 Require Import Interface.
+Require Import Sail.Base.
 Require Export SailArmInst_types.
+Require Import Coq.Reals.Reals.
+From RecordUpdate Require Import RecordSet.
 
+Inductive regval  :=
+  | Regval_unknown : regval
+  | Regval_vector : list regval -> regval
+  | Regval_list : list regval -> regval
+  | Regval_option : option regval -> regval
+  | Regval_bool : bool -> regval
+  | Regval_int : Z -> regval
+  | Regval_real : R -> regval
+  | Regval_string : string -> regval
+  | Regval_bitvector z : mword z -> regval
+  | Regval_struct : list (string * regval) -> regval.
+
+#[global] Instance FullAddress_eta : Settable _ :=
+  settable! Build_FullAddress <FullAddress_paspace; FullAddress_address>.
 
 Module Arm <: Arch.
   Definition reg := string.
-  Definition reg_type := register_value.
+  Definition reg_type := regval.
   Definition va_size := 64%N.
   Definition pa := FullAddress.
   Definition arch_ak := arm_acc_type.
