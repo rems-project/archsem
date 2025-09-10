07ca57df7d9e6d684ddbe75f47144a950b5aca8e
Brian Campbell <Brian.Campbell@ed.ac.uk>
2025-09-10 15:58:20 +0100

    Update sequential test based on isla-litmus-dump

--- ArchSemArm/tests/ArmSeqModelIslaTest.v
+++ ArchSemArm/tests/ArmSeqModelIslaTest.v
@@ -3,7 +3,6 @@ From ArchSemArm Require Import ArmInst IslaInit.
 From ASCommon Require Import CResult.
 
 
-
 Open Scope stdpp.
 Open Scope bv.
 
@@ -29,47 +28,25 @@ Definition regs_extract (a : Model.Res.t ∅ 1) : result string (list (sigT reg_
   end.
 
 (** Common configuration from Isla config files to run at EL0 in AArch64 mode *)
-Definition common_init_regs :=
-  isla_init_regs
+Definition add_common_regs pre :=
+  pre
   |> reg_insert PCUpdated false
-  |> reg_insert EDESR 0x0
-  |> reg_insert ShouldAdvanceIT false
-  |> reg_insert ShouldAdvanceSS false
-  |> reg_insert BTypeCompatible false
-  |> reg_insert __InstructionStep false
   |> reg_insert SPESampleInFlight false
   |> reg_insert InGuardedPage false
   |> reg_insert __supported_pa_size 52%Z
-  |> reg_insert __tlb_enabled false
-  |> reg_insert __ExclusiveMonitorSet false
-  |> reg_insert __ThisInstrEnc inhabitant
-  |> reg_insert __ThisInstr 0x0
-  |> reg_insert __currentCond 0x0
   |> reg_insert SEE 0x0%Z
-  |> reg_insert BTypeNext 0x0
-
-  (* Registers that get read due to extra supported features, but which are irrelevant
-     because the feature isn't used. *)
-  |> reg_insert MAIR2_EL1 inhabitant
-  |> reg_insert PIR_EL1 inhabitant
-  |> reg_insert PIRE0_EL1 inhabitant
-  |> reg_insert S2PIR_EL2 inhabitant
 .
 
 (** We test against the sail-arm semantic, with non-determinism disabled *)
 Definition arm_sem := sail_arm_sem false.
 
-Module STRLDR. (* STR X2, [X1, X0]; LDR X0, [X1, X0] at 0x500, using address 0x1100 *)
+Module STRLDR. (* STR X2, [X1, X0]; LDR X0, [X1, X0] with address in x1, zero in x0 *)
   Definition init_reg : registerMap :=
-    common_init_regs
-    |> reg_insert PC 0x400000
-    |> reg_insert R0 0x1000
-    |> reg_insert R1 0x100
-    |> reg_insert R2 0x2a.
+    nth 0 isla_threads_init_regs dmap_empty
+    |> add_common_regs.
 
   Definition init_mem : memoryMap:=
-    isla_init_mem
-    |> mem_insert 0x1100 8 0x0. (* Memory need to exists to be written to *)
+    isla_init_mem.
 
   Definition termCond : terminationCondition 1 :=
     (λ tid rm, reg_lookup PC rm =? Some (0x400008 : bv 64)).
