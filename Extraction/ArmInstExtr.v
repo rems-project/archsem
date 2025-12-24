319d88b0a2e2a626035217bf4dd02d496778742a
Yeji Han <yeji.han@sf.snu.ac.kr>
2025-12-24 21:22:05 +0900

    Extract VMPromising Model - Pass example tests: MP (mpvm.toml) and LDRPT (ldrpt.toml)

--- Extraction/ArmInstExtr.v
+++ Extraction/ArmInstExtr.v
@@ -53,4 +53,5 @@ Set Extraction Output Directory ".".
 (* Definition umProm_cert_c := UMPromising_cert_c. *)
 
 #[warnings="-extraction-remaining-implicit,-extraction-reserved-identifier"]
-Separate Extraction sequential_modelc sail_tiny_arm_sem Arm UMPromising_cert_c.
+Separate Extraction Arm sail_tiny_arm_sem
+  sequential_modelc UMPromising_cert_c VMPromising_cert_c.
