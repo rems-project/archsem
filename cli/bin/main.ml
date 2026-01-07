972f322dd5d9bccb8eb8d6e67b7b5edcb202c353
Yeji Han <yeji.han@sf.snu.ac.kr>
2026-01-07 22:07:59 +0900

    Add VM tests

--- cli/bin/main.ml
+++ cli/bin/main.ml
@@ -2,16 +2,22 @@ open Archsem
 
 let () =
   if Array.length Sys.argv < 3 then
-    Printf.eprintf "Usage: %s <model: seq|ump> <test_file.toml>\n" Sys.argv.(0)
+    Printf.eprintf "Usage: %s <model: seq|ump|vmp> <test_file.toml>\n" Sys.argv.(0)
   else
     let model_str = Sys.argv.(1) in
     let filename = Sys.argv.(2) in
+
+    (* Wrap vmp model with default parameters: debug=false, mem_strict=false, bbm_check=true *)
+    let vmp_model fuel term state =
+      vmProm_model fuel false false true term state
+    in
 
     let model =
       match model_str with
       | "seq" -> seq_model
       | "ump" -> umProm_model
-      | _ -> failwith ("Unknown model: " ^ model_str ^ ". Supported: seq, ump")
+      | "vmp" -> vmp_model
+      | _ -> failwith ("Unknown model: " ^ model_str ^ ". Supported: seq, ump, vmp")
     in
 
     let success = Archsem_test.Litmus_runner.run_litmus_test model filename in
